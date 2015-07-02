adf1db6582764b1b4186e72c5951f526a3091513
mohamedrias <mohamedrias@gmail.com>
2015-07-02 23:18:43 +0530

    Formatted using go fmt

--- src/7.ControlFlow/if.go
+++ src/7.ControlFlow/if.go
@@ -7,30 +7,29 @@ import (
 func main() {
 	//If statements are same as C language but doesn't need to include () braces
 	x := "hi there"
-	if x=="hi there" {
+	if x == "hi there" {
 		fmt.Println("Hi There :P")
 	}
-	
+
 	i := 20
 	/*
 		If statements can take optional initialization parameters as well.
 		This is different compared to the traditional C approach
 	*/
-	if y :=10; i < y {
+	if y := 10; i < y {
 		fmt.Printf("%d is less than %d\n", i, y)
 	} else {
 		fmt.Printf("%d is greater than %d\n", i, y)
 	}
-	
-	
+
 	/**
-		Can initialize multiple arguments as well.
-		It keeps the if condition very simple
+	Can initialize multiple arguments as well.
+	It keeps the if condition very simple
 	*/
-	
-	if x, y := 21, 12; x%3 ==0 && y%7 == 0 {
+
+	if x, y := 21, 12; x%3 == 0 && y%7 == 0 {
 		fmt.Printf("X / y is not a factor")
-	} else if x%7 ==0 && y%3 ==0 {
+	} else if x%7 == 0 && y%3 == 0 {
 		fmt.Printf("Both numbers are modulus of 3 & 7")
 	}
 }
