7bcb86cf67b3ba1763138a6dd4e150b3b7a55640
mohamedrias <mohamedrias@gmail.com>
2015-07-04 19:45:21 +0530

    Updated struct

--- src/10.User-defined types/Struct/struct.go
+++ src/10.User-defined types/Struct/struct.go
@@ -1,1 +1,67 @@
 // Struct - user defined data types
+
+package main
+
+import "fmt"
+
+func main() {
+	
+	/* 
+		struct declaration
+		Use capital case to identify structs easily.
+	*/
+	type Employee struct {
+		id int
+		name string
+		company string
+	}
+	
+	/*
+		For using employee struct, use pointer notation
+	*/
+	
+	rias := Employee{
+		name: "Rias",
+		id: 4,
+		company: "TCS",	
+	}
+	
+	/*
+		Printing struct object
+	*/
+	
+	fmt.Println(rias)
+	
+	/*
+		To Copy one struct into another	
+	*/
+	
+	riasNew := &rias
+	
+	fmt.Println(riasNew)
+	
+	riasNew.name = "Mohamed Rias"
+	
+	fmt.Println(riasNew, rias)
+	/*
+		Other ways of refering to the struct object
+	*/
+	
+	fmt.Println(Employee{2,"Rafi", "3i Infotech"})
+	
+	
+	fmt.Println(Employee{id: 3, name: "Hiren", company: "Babajob"})
+	
+	
+	/*
+		Traditional struct declaration
+	*/
+	
+	alex := Employee{}
+	alex.id = 10
+	alex.name = "Alex Rattray"
+	alex.company = "Babajob"
+	
+	fmt.Println(alex)
+	
+}
---
+++ src/10.User-defined types/Struct/struct.md
@@ -0,0 +1,3 @@
+#struct
+
+Structs are used to define the user defined objects. Each struct can have custom properties.
