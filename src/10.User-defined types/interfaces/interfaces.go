40841342fda82df5799ddec47ce51ec2023f689d
mohamedrias <mohamedrias@gmail.com>
2015-07-04 22:14:06 +0530

    Fixed interface changes

--- src/10.User-defined types/interfaces/interfaces.go
+++ src/10.User-defined types/interfaces/interfaces.go
@@ -1,24 +1,40 @@
 package main
 
-import _ "fmt"
+import (
+	"fmt"
+)
+
+/**
+	Interface
+	type interface name followed by interface keyword
+*/
+type AwesomeDeveloper interface {
+	// Interface API methods
+	Codes()
+}
+
+// Random struct type
+type Developer struct {
+	name string
+}
+
+/*
+	Implement the interface.
+	Need to use value Developer instead of *Developer for it to work
+*/
+func (dev Developer) Codes() {
+	fmt.Println("Awesome", dev.name)
+}
+
+/*
+	Generic methods to use across multiple interfaces
+*/
+func judge(d AwesomeDeveloper) {
+	d.Codes()
+}
 
 func main() {
-	
-	type awesomeDeveloper interface {
-		eats()
-		codes()
-		sleeps()
-		repeats()
-	}
-	
-	type dev struct {
-		name string
-	}
-	
-	func (d *dev) eats() string {
-		fmt.Println("Rias")
-		return "Rias"
-	}
-	
-	//TODO: Need to refactor. It's giving error :(
+	rias := Developer{"Rias"}
+	judge(rias)
+	rias.Codes()
 }
