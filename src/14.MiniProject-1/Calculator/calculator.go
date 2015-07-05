6d7116a0cd663d039eb1bd3490ed51133da01258
mohamedrias <mohamedrias@gmail.com>
2015-07-05 19:41:39 +0530

    Created a mini project - Calculator to show package export

---
+++ src/14.MiniProject-1/Calculator/calculator.go
@@ -0,0 +1,11 @@
+package calculator
+
+func Add(a, b int) int {
+	return a+b
+}
+
+
+func Substract(a,b int) (c int) {
+	c = a-b
+	return
+}
---
+++ src/14.MiniProject-1/miniproject.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+import calc "14.MiniProject-1/Calculator"
+
+func main() {
+	fmt.Println(calc.Add(4,5))
+	
+	fmt.Println(calc.Substract(54,5))
+}
